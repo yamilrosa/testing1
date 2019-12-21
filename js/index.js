9bcf18f7057fa32fa353009a672902068af308f6
Yams <yamilrosa@live.com>
2019-12-21 16:13:17 -0500

    added petfinder client

--- js/index.js
+++ js/index.js
@@ -1,3 +1,64 @@
+import fetchJsonp from 'fetch-jsonp';
+import { Client } from "@petfinder/petfinder-js";
+
+
 if (module.hot) {
     module.hot.accept()
   }
+
+  const petForm = document.querySelector('#pet-form');
+
+  petForm.addEventListener('submit', fetchAnimals);
+
+  //Fetch Animals From API
+  function fetchAnimals(e) {
+    e.preventDefault();
+    
+    //Get User Input
+    const animal = document.querySelector('#animal').value;
+    const zip = document.querySelector('#zip').value;
+
+    console.log(animal);
+    console.log(zip)
+
+    //API Key & API sS
+    const apiKey = 'Jl0MmQgBZsoEUzoCQTGrlsedOmu1qscsN1XqfiuoKDIyRE6Ruc';
+    const apiSecret = 'NAMylQ5UTbSRJYD0xJD6MYGONSsScruhPjPxeyA7'
+
+
+    //Fetch Pets
+    const client = new Client({apiKey: apiKey, secret: apiSecret});
+
+    client.animal.search({type: animal})
+      .then(response => {
+      // Do something with resp.data.animals
+        console.log(response.data.animals)
+      })
+      .catch(function (error) {
+          // Handle the error
+          console.log(error)
+      });
+
+
+
+
+
+
+
+    //Fetch Pets
+    // fetchJsonp(`http://api.petfinder.com/pet.finder?format=json&key=${key}&animal=${animal}&location=${zip}&callback=callback`, {
+    //   jsonpCallbackFunction: 'callback'
+    // })
+    // .then(res => res.json())
+    // .then(data => console.log(data))
+    // .then(err => console.log(err))
+
+  }
+
+  //JSONP Callback
+
+  function callback(data) {
+    console.log(data);
+  }
+
+
